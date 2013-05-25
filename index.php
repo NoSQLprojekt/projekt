1aa90fdb8d265cebd186505e58540beea7d4cd08
Aneta Budner <aneta57@o2.pl>
2013-05-25 14:31:05 +0200

    dodanie index.php i style.css

---
+++ index.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Książka adresowa</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="#">Strona Główna</a></li>
+			<li><a href="#">Statystyki</a></li>
+			<li><a href="#">Logowanie</a></li>
+		</ul>
+	</div>
+	<!-- end #menu -->
+</div>
+
+<div id="wrapper">
+	<!-- end #header -->
+	<div id="page">
+		<div id="page-bgtop">
+			<div id="page-bgbtm">
+				<div id="content">
+					<div class="post">
+						
+							<h2 class="title"><a href="#">Witamy na naszej stronie</a></h2>
+							<div style="clear: both;">&nbsp;</div>
+							<div class="entry">
+								<p>Aby zacząć korzystać z książki adresowej zaloguj się lub zarejestruj.</p>
+							</div>
+						
+					</div>
+					
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+				<!-- end #content -->
+				<div id="sidebar">
+					<img src="Address-book.png" width="400" height="400" alt="" />
+				</div>
+				<!-- end #sidebar -->
+				<div style="clear: both;">&nbsp;</div>
+			</div>
+		</div>
+	</div>
+	<!-- end #page -->
+</div>
+
+</body>
+</html>
