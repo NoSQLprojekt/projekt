2a70c815a6a58c992fc82043b98ee414c7535766
Misio <misiom1@gmail.com>
2013-05-26 14:19:11 +0200

    form change

--- register.php
+++ register.php
@@ -5,17 +5,17 @@
 			
 			<div class="entry">
 					<h2 class="title">Rejestracja</h2>
-								<form method="post" action="save.php?add">
+								<form method="post" action="index.php?save&addUser">
 									<div id="adduser">
 										
 										<p>Login:</p>
-										<input type="text" name="imie" id="login-text" value="" />
+										<input type="text" name="login" id="login-text" value="" />
 										<p>Email:</p>
-										<input type="text" name="nazwisko" id="login-text" value="" />
+										<input type="text" name="email" id="login-text" value="" />
 										<p>Hasło:</p>
-										<input type="text" name="adres" id="login-text" value="" />
+										<input type="text" name="haslo" id="login-text" value="" />
 										<p>Powtórz hasło:</p>
-										<input type="text" name="telefon" id="login-text" value="" />
+										<input type="text" name="haslo2" id="login-text" value="" />
 										
 										
 										<p class="submit">  
